9ad9f74d681e9ded6294fdfa153bbe07cbbfad29
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-06-09 11:30:02 +0100

    changed to Ifort and updated docs

--- src/features/linter-provider.ts
+++ src/features/linter-provider.ts
@@ -25,7 +25,9 @@ export default class FortranLintingProvider {
 		let diagnostics: vscode.Diagnostic[] = [];
 		let options = vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
 		let args = ['', textDocument.fileName];
-		let childProcess = cp.spawn('gfortran', ['-cpp', '-fsyntax-only', '-Wall','-fdiagnostics-show-option', textDocument.fileName]);
+		// let childProcess = cp.spawn('gfortran', ['-cpp', '-fsyntax-only', '-Wall','-fdiagnostics-show-option', textDocument.fileName]);
+		let childProcess = cp.spawn('ifort', ['-cpp', '-fsyntax-only', '-Warn all', textDocument.fileName]);
+
 		// let childProcess = cp.spawn('ps', ['ax']);
 		if (childProcess.pid) {
 			childProcess.stdout.on('data', (data: Buffer) => {

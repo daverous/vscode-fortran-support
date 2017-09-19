3a3ef19618e8963fd03919172cd5a8d935157316
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-09-19 09:02:47 -0400

    handled multiple line variables

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -4,7 +4,7 @@ import { Range, Position, TextDocument, FormattingOptions, TextEditorOptions } f
 
 export const functionRegEx = /^(?!end)([a-zA-Z](\([\w.=]+\))*)*\s*function\s*([a-zA-Z0-9_]*)\s*\(*(\s*[a-zA-Z_][a-zA-Z0-9_,\s]*)*\s*\)*\s*(result\([a-zA-Z_][\w]*\))*/ig;
 export const subroutineRegEx = /(^(?!end\s))([a-zA-Z]{1,}\s+)*subroutine\s*([a-zA-Z][a-zA-Z0-9_]*)\s*\(*(\s*[a-zA-Z][a-zA-z0-9_,\s]*)*\s*\)*/ig;
-export const varibleDecRegEx = /^(([a-zA-Z]{1,})(\((kind=)*([a-zA-Z0-9]{1,})\))?(,\s*[a-zA-Z0-9]{1,}(\(.*\))?)*)\s*::\s*([a-zA-Z_][a-zA-Z0-9_]*)/ig;
+export const varibleDecRegEx = /^(([a-zA-Z]{1,})(\((kind=)*([a-zA-Z0-9]{1,})\))?(\s*,\s*[a-zA-Z0-9]{1,}(\(.*\))?)*)\s*::(\s*([a-zA-Z_][a-zA-Z0-9_]{1,})(,\s*([a-zA-Z0-9_]*))*)/ig;
 export const typeDecRegEx = /^type(,\s*[a-zA-Z0-9]{1,}(\(.*\))?)*\s*::\s*([a-zA-Z_][a-zA-Z0-9_]*)/ig;
 export const interfaceRegEx = /^interface\s*([a-zA-Z][a-zA-Z0-9_]*)\s*\(*(\s*[a-zA-Z][a-zA-z0-9_,\s]*)*\s*\)*/ig;
 export const endRegex = /^((?!(end\s*block|end\s*if|end\s*select|end\s*do|end\s*type))end)/ig; // regex means that ends won't work for inside
@@ -28,16 +28,16 @@ export const caseRegex = /^(case\s*\(.*\))$/;
 
 export abstract class ProgramPart {
     name: string;
-    
+
     lineNumber?: number;
     parent?: string;
     parentObject?: ProgramPart;
 }
-export interface Variable extends ProgramPart{
+export interface Variable extends ProgramPart {
     type?: string;
 }
 
-export interface Class extends ProgramPart{
+export interface Class extends ProgramPart {
     usages: Package[];
     name: string;
     // TODO maybe a list of variables
@@ -55,7 +55,7 @@ export interface Subroutine extends ProgramPart {
     lineNumber: number;
 }
 
-export interface Function extends ProgramPart {    
+export interface Function extends ProgramPart {
     usages: Package[]
     args: Variable[];
     return: Variable; // function is a subroutine with return type
@@ -70,77 +70,83 @@ export enum MethodType {
 
 
 export function getRange(document: TextDocument): Range {
-  let start: Position = new Position(0, 0);
-  let endLine = document.lineCount - 1;
-  let end: Position = new Position(endLine, document.lineAt(endLine).text.length);
-  return new Range(start, end);
+    let start: Position = new Position(0, 0);
+    let endLine = document.lineCount - 1;
+    let end: Position = new Position(endLine, document.lineAt(endLine).text.length);
+    return new Range(start, end);
 }
 
 
-export function getDeclaredSubroutinesVariablesAndFunctions(document: vscode.TextDocument):[Subroutine[],Function[],Variable[],Class[],Package[]]{
-    
-        let lines = document.lineCount;
-        let subroutines = [];
-        let funcs = [];
-        let vars = [];
-        let classes = [];
-        let packs = [];
-        let curBlock = [];
-        for (let i = 0; i < lines; i++) {
-            let line: vscode.TextLine = document.lineAt(i);
-            if (line.isEmptyOrWhitespace) continue;
-            let subFuct = parseSubroutineAndFunctions(line.text,curBlock[curBlock.length-1] );
+export function getDeclaredSubroutinesVariablesAndFunctions(document: vscode.TextDocument): [Subroutine[], Function[], Variable[], Class[], Package[]] {
+
+    let lines = document.lineCount;
+    let subroutines = [];
+    let funcs = [];
+    let vars = [];
+    let classes = [];
+    let packs = [];
+    let curBlock = [];
+    for (let i = 0; i < lines; i++) {
+        let line: vscode.TextLine = document.lineAt(i);
+        if (line.isEmptyOrWhitespace) continue;
+        let subFuct = parseSubroutineAndFunctions(line.text, curBlock[curBlock.length - 1]);
 
 
-           
-            if (subFuct != null) {
-                if (subFuct.what =="end") {
-                    // pop item off the top of the stack 
-                    curBlock.pop();
-                }
-                else if(subFuct.what == "program"){
-                    curBlock.push(subFuct.name);
-                    packs.push({...subFuct, parent  : subFuct.in,lineNumber: i });
-                }
-                else if(subFuct.what == "module"){
-                    curBlock.push(subFuct.name);
-                    packs.push({...subFuct, parent  : subFuct.in,lineNumber: i });
-                }
-                else if(subFuct.what == "inter"){
-                    curBlock.push(subFuct.name);
-                    classes.push({...subFuct, parent  : subFuct.in,lineNumber: i });
-                }
-                else if(subFuct.what == "sub"){
-                    curBlock.push(subFuct.name);
-                    subroutines.push({...subFuct, parent  : subFuct.in,lineNumber: i });
-                }
-                else if (subFuct.what == "func") {
-                    curBlock.push(subFuct.name);
-                    funcs.push({...subFuct,parent  : subFuct.in, lineNumber: i });
-                }
-                else if (subFuct.what == "var") {
-                    vars.push({...subFuct, lineNumber: i , parent  : subFuct.in, type : subFuct.type});
-                }
-                else if (subFuct.what == "class") {
-                    curBlock.push(subFuct.name);
-                    classes.push({...subFuct, lineNumber: i , parent  : subFuct.in, type : subFuct.type});
+
+        if (subFuct != null) {
+            if (subFuct.what == "end") {
+                // pop item off the top of the stack 
+                curBlock.pop();
+            }
+            else if (subFuct.what == "program") {
+                curBlock.push(subFuct.name);
+                packs.push({ ...subFuct, parent: subFuct.in, lineNumber: i });
+            }
+            else if (subFuct.what == "module") {
+                curBlock.push(subFuct.name);
+                packs.push({ ...subFuct, parent: subFuct.in, lineNumber: i });
+            }
+            else if (subFuct.what == "inter") {
+                curBlock.push(subFuct.name);
+                classes.push({ ...subFuct, parent: subFuct.in, lineNumber: i });
+            }
+            else if (subFuct.what == "sub") {
+                curBlock.push(subFuct.name);
+                subroutines.push({ ...subFuct, parent: subFuct.in, lineNumber: i });
+            }
+            else if (subFuct.what == "func") {
+                curBlock.push(subFuct.name);
+                funcs.push({ ...subFuct, parent: subFuct.in, lineNumber: i });
+            }
+            else if (subFuct.what == "var") {
+                let names = subFuct.name.split(',');
+
+                for (let n of names) {
+                    subFuct.name = n;
+                    vars.push({ ...subFuct, lineNumber: i, parent: subFuct.in, type: subFuct.type });
+
                 }
             }
-        } 
-        return [subroutines,funcs,vars, classes,packs];
+            else if (subFuct.what == "class") {
+                curBlock.push(subFuct.name);
+                classes.push({ ...subFuct, lineNumber: i, parent: subFuct.in, type: subFuct.type });
+            }
+        }
     }
+    return [subroutines, funcs, vars, classes, packs];
+}
 
 
-export const parseSubroutineAndFunctions = (line: string, curSubName : string) => {
-    
-        return _parse_new(line,curSubName);
-    }
+export const parseSubroutineAndFunctions = (line: string, curSubName: string) => {
+
+    return _parse_new(line, curSubName);
+}
 
 export function matchBlock(line: string): boolean {
     line = line.trim();
 
     if (line.match(functionRegEx)) return true;
-        
+
     if (line.match(subroutineRegEx)) return true;
     if (line.match(containsRegex)) return true;
     // if (line.match(typeDecRegEx)) return true;
@@ -160,66 +166,67 @@ export function matchBlock(line: string): boolean {
 }
 
 export const _parse_new = (line: string, parent: string) => {
-        line = line.trim()
+    line = line.trim()
 
-        let temp = line.match(functionRegEx)
-        if (line.match(endRegex)) {
-            return {
-                what: "end",
-                name: "",
-                args: "",
-                type : "",
-                in: ""
-            }
+    let temp = line.match(functionRegEx)
+    if (line.match(endRegex)) {
+        return {
+            what: "end",
+            name: "",
+            args: "",
+            type: "",
+            in: ""
         }
-        
-        else if (line.match(functionRegEx)) {
-            let [attr, kind_descriptor, name, argsstr, result] = functionRegEx.exec(line).slice(1, 5);
-            let args = (argsstr) ? parseArgs(argsstr) : [];
-            return {
-                what: "func",
-                name: name,
-                args: args,
-                type: kind_descriptor,
-                in: parent
-            };
-        } else if (line.match(subroutineRegEx)) {
-            let temp =subroutineRegEx.exec(line);
-            let name = temp[3];
-            let argsstr = temp[4];
-            // l/et [name, argsstr] = temp.slice(1);
-            let args = (argsstr) ? parseArgs(argsstr) : [];
-            return {
-                what: "sub",
-                name: name,
-                args: args,
-                type : "",
-                in: parent // add a field to show if the variable is in
-            };
-        } else if(line.match(typeDecRegEx)) {
-            
-            let [matchExp, type, name ] = typeDecRegEx.exec(line);
-            return {what: "class", name: name, type: type, in: parent};
-        } else if(line.match(interfaceRegEx)) {
-            
-            let [matchExp, name ] = interfaceRegEx.exec(line);
-            return {what: "inter", name: name, type: "", in: parent};
-        } else if(line.match(varibleDecRegEx)) {
-            
-            let [matchExp, temp1, type, props,temp,kind,test,testj, name ] = varibleDecRegEx.exec(line)
-            return {what: "var", name: name, type: type, in: parent};
-        } else if(line.match(programRegex)) {
-            
-            let [matchExp, name ] = programRegex.exec(line);
-            return {what: "program", name: name, type: "", in: parent};
-        }
-        else if(line.match(moduleRegex)) {
-            
-            let [matchExp, name ] = moduleRegex.exec(line);
-            return {what: "module", name: name, type: "", in: parent};
-        }  
-    
     }
+
+    else if (line.match(functionRegEx)) {
+        let [attr, kind_descriptor, name, argsstr, result] = functionRegEx.exec(line).slice(1, 5);
+        let args = (argsstr) ? parseArgs(argsstr) : [];
+        return {
+            what: "func",
+            name: name,
+            args: args,
+            type: kind_descriptor,
+            in: parent
+        };
+    } else if (line.match(subroutineRegEx)) {
+        let temp = subroutineRegEx.exec(line);
+        let name = temp[3];
+        let argsstr = temp[4];
+        // l/et [name, argsstr] = temp.slice(1);
+        let args = (argsstr) ? parseArgs(argsstr) : [];
+        return {
+            what: "sub",
+            name: name,
+            args: args,
+            type: "",
+            in: parent // add a field to show if the variable is in
+        };
+    } else if (line.match(typeDecRegEx)) {
+
+        let [matchExp, type, name] = typeDecRegEx.exec(line);
+        return { what: "class", name: name, type: type, in: parent };
+    } else if (line.match(interfaceRegEx)) {
+
+        let [matchExp, name] = interfaceRegEx.exec(line);
+        return { what: "inter", name: name, type: "", in: parent };
+    } else if (line.match(varibleDecRegEx)) {
+
+        let [matchExp, temp1, type, props, temp, kind, test, testj, name] = varibleDecRegEx.exec(line)
+        // name might need split
+        return { what: "var", name: name, type: type, in: parent };
+    } else if (line.match(programRegex)) {
+
+        let [matchExp, name] = programRegex.exec(line);
+        return { what: "program", name: name, type: "", in: parent };
+    }
+    else if (line.match(moduleRegex)) {
+
+        let [matchExp, name] = moduleRegex.exec(line);
+        return { what: "module", name: name, type: "", in: parent };
+    }
+
+}
 
 
 
